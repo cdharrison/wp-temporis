6c980619f0e02d960d35808bf42e869ef1583ad6
@cdharrison <chris@cdharrison.com>
2013-07-17 15:25:12 -0400

    Create wp-temporis.php

---
+++ wp-temporis.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: WP-Temporis
+Plugin URI: http://github.com/cdharrison/wp-temporis/
+Description: A simple, responsive timeline-based carousel.
+Version: 0.1
+Author: Chris Harrison/Morris Media Network
+Author URI: http://morrismedianetwork.com
+License: GPL 2.0
+*/
+
+
+/*  Copyright 2013 Chris Harrison  (email : chris@cdharrison.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+?>
